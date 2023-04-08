b511363bd75662e9a78b331e8454fb1ed9701336
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-09 03:16:47 +0900

    feat: #로그인 / 회원가입  회원가입 폼 마크업

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function SignUp() {
-	return <div>SignUp</div>;
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const signUpHandler = async (e: React.FormEvent) => {
+		e.preventDefault();
+	};
+	return (
+		<div>
+			<form onSubmit={signUpHandler} className="flex flex-col">
+				<label htmlFor="id">아이디 *</label>
+				<input
+					id="id"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					data-testid="email-input"
+					type="email"
+				/>
+				<label htmlFor="password">비밀번호 *</label>
+				<input
+					id="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					data-testid="password-input"
+					minLength={8}
+				/>
+				<button
+					disabled={Boolean(!email.includes('@') || password.length < 8)}
+					type="submit"
+					data-testid="signup-button"
+				>
+					회원가입
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default SignUp;
