aef212ca9cd259d42834ad4c6115b2e7b924da27
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-09 05:21:14 +0900

    feat: #TODO LIST 할일체크,삭제 api 연동

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -5,6 +5,14 @@ import React, { useEffect, useState } from 'react';
 function Todo() {
 	const [newTodo, setNewTodo] = useState<string>('');
 	const [todos, setTodos] = useState<ITodo[]>([]);
+	const getTodosHandler = async () => {
+		try {
+			const { data } = await client.get('/todos');
+			setTodos(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 	const addNewTodoHandler = async (e: React.FormEvent) => {
 		e.preventDefault();
 		if (!newTodo.trim()) return;
@@ -12,16 +20,36 @@ function Todo() {
 			const response = await client.post('/todos', {
 				todo: newTodo,
 			});
-			console.log(response);
+			setNewTodo('');
+			getTodosHandler();
 		} catch (error) {
+			getTodosHandler();
 			console.error(error);
 		}
 	};
-	const getTodosHandler = async () => {
+
+	const deleteTodoHandler = async (id: ITodo['id']) => {
 		try {
-			const { data } = await client.get('/todos');
-			setTodos(data);
+			const res = await client.delete(`/todos/${id}`);
+			console.log(res);
+			let temp = [...todos];
+			temp = temp.filter((item) => item.id !== id);
+			setTodos(temp); //optimistic ui구현
 		} catch (error) {
+			getTodosHandler();
+			console.error(error);
+		}
+	};
+	console.log(todos);
+	const checkTodoHandler = async (id: ITodo['id'], todo: ITodo['todo'], isCompleted: ITodo['isCompleted']) => {
+		try {
+			const res = await client.put(`/todos/${id}`, {
+				todo,
+				isCompleted,
+			});
+			console.log(res);
+		} catch (error) {
+			getTodosHandler();
 			console.error(error);
 		}
 	};
@@ -39,10 +67,18 @@ function Todo() {
 			{todos.map(({ id, todo, isCompleted }) => (
 				<li key={id}>
 					<label>
-						<input type="checkbox" />
+						<input
+							defaultChecked={isCompleted}
+							onChange={(e) => checkTodoHandler(id, todo, e.target.checked)}
+							type="checkbox"
+						/>
 						<span>{todo}</span>
 					</label>
-					<input defaultChecked={isCompleted} data-testid="modify-input" />
+					<input data-testid="modify-input" />
+					<button data-testid="modify-button">수정</button>
+					<button onClick={() => deleteTodoHandler(id)} data-testid="delete-button">
+						삭제
+					</button>
 					<button data-testid="submit-button">제출</button>
 					<button data-testid="cancel-button">취소</button>
 				</li>
